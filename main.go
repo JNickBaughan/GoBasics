f561af6e22c096bb7941addc30b0e026b8bd9a62
JNickBaughan <JNickBaughan@gmail.com>
2020-06-28 18:38:58 -0400

    add function to deal random card

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	var cards = []string{"Five of Diamonds", "Five of Spades", "Five of Hearts", "Five of Clubs"}
+	cards = append(cards, "Six of Diamonds")
+	cards = append(cards, "Six of Spades")
+	cards = append(cards, "Six of Hearts")
+	cards = append(cards, "Six of Clubs")
+
+	card := dealNewCard(cards)
+	fmt.Println(card)
+
+}
+
+func dealNewCard(deck []string) string {
+	min := 0
+	max := len(deck) - 1
+	rand.Seed(time.Now().UnixNano())
+	return deck[rand.Intn(max-min)+min]
 }
